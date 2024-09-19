280684567ea0cc6278d2814e94e7ae307a995db2
Vladimir <133875471+Vovamojet@users.noreply.github.com>
2024-09-19 22:27:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 
-//фокус на вводе
 window.onload = function() {
     document.getElementById('inputText').focus();
 };
 
 
-// Конвертация текста
 document.getElementById('convertButton').addEventListener('click', function() {
     let inputText = document.getElementById('inputText').value;
     let outputText = inputText.split('\n').filter(Boolean).join(',');
     document.getElementById('outputText').value = outputText;
 });
 
-// Копирование результата в буфер обмена
 document.getElementById('copyButton').addEventListener('click', function() {
     let outputText = document.getElementById('outputText');
-    
-    // Выбираем текст для копирования
     outputText.select();
-
-    // Копируем текст в буфер обмена
     document.execCommand('copy');
+});
+
+document.getElementById('clearButton').addEventListener('click', function() {
+    document.getElementById('inputText').value = '';
+    document.getElementById('inputText').focus();
 });
